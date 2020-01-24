bcf38276b3e7dcf4c8642ba558174b798d70f0f5
macstudent <kamal777682>
2020-01-23 21:13:19 -0500

    reverse string

--- src/main/java/com/lambton/Array.java
+++ src/main/java/com/lambton/Array.java
@@ -2,8 +2,7 @@ package com.lambton;
 
 
 
-public class Array
-{
+public class Array {
 
     public static void main(String[] args) {
         int a[];
@@ -41,52 +40,82 @@ public class Array
         System.out.println("Using Compact for(:)");
 
 
+        // class assignment
 
-            // class assignment
+        int i, j, max, min, sum;
+        float avg;
+        max = 0;
+        min = 0;
+        sum = 0;
+        int ar[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-            int i,j, max, min, sum;
-            float avg;
-            max = 0;
-            min = 0;
-            sum = 0;
-            int ar[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Display Array values: ");
 
-            System.out.println("Display Array values: ");
-
-            for(i=0;i< ar.length ; i++)
-                System.out.println(ar[i]);
-
-            for(j=0;j< ar.length;j++)
-            {
-                sum+=j;
-            }
-            System.out.println("Sum: " + sum);
-
-
-            avg = (float)sum/ar.length;
-            System.out.println("Average : " + avg);
-
-            for(i=0;i< ar.length; i++)
-            {
-                max= ar[0];
-                if(max<ar[i])
-                    max=ar[i];
-
-            }
-            System.out.println("Maximum value in array : " +max);
-        for(i=0;i< ar.length; i++)
-        {
-            min= ar[0];
-            if(min >ar[i])
-                min =ar[i];
-
-        }
-        System.out.println("Minimum value in array : " +min);
-
-        for(i=ar.length-1;i >=0; i--)
-
+        for (i = 0; i < ar.length; i++)
             System.out.println(ar[i]);
 
+        for (j = 0; j < ar.length; j++) {
+            sum += j;
+        }
+        System.out.println("Sum: " + sum);
+
+
+        avg = (float) sum / ar.length;
+        System.out.println("Average : " + avg);
+
+        for (i = 0; i < ar.length; i++) {
+            max = ar[0];
+            if (max < ar[i])
+                max = ar[i];
+
+        }
+        System.out.println("Maximum value in array : " + max);
+        for (i = 0; i < ar.length; i++) {
+            min = ar[0];
+            if (min > ar[i])
+                min = ar[i];
+
+        }
+        System.out.println("Minimum value in array : " + min);
+
+        for (i = ar.length - 1; i >= 0; i--)
+
+            System.out.println(ar[i]);
+        String s="";
+
+        stringArrayHandling();
+        revString(s);
+    }
+
+    public static void stringArrayHandling()
+    {
+        String s[];
+        s = new String[10];
+        s[0] = "India";
+        s[1] = new String("India");
+        s[2] = "Nepal";
+        s[3] = "Toronto";
+        s[4] = "New York";
+        s[5] = "Thomas";
+        s[6] = "Jyothi";
+        s[7] = "James";
+        s[8] = "Raelyn";
+        s[9] = "Remy";
+        for (int i1 = 0; i1 < s.length; i1++) {
+            String rev = revString(s[i1]);
+            System.out.println(" " + s[i1] + "<->" + rev);
+                    }
+                    }
+public static String revString (String str)
+        {
+        char c [] = str.toCharArray();
+        String rev="";
+        for (int i = c.length-1; i>=0; i--)
+        {
+        rev= rev+c[i];
+        }
+        return rev;
         }
 
-    }
+
+        }
